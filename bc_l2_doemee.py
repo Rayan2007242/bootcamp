dd8bf490278e41fe94fd6982e0d3e31fcdd83a24
Rayan2007242 <rayan3062007@gmail.com>
2024-09-02 12:51:34 +0200

    Add files via upload

---
+++ bc_l2_doemee.py
@@ -0,0 +1,20 @@
+# #Krijgt dit dezelfde uitkomst? 
+# print( 5*2 -3+4/2 )
+# print( 5*2 - 3+4 / 2 )
+
+# #En hoe zit het met deze?
+# print( (5*2) - (3+4) /2 )
+# print( ((5*2) -(3+4)) / 2 )
+
+# #Bekijk de volgende code, wat denk je dat eruit komt?
+# print( (431 / 100) * 100 )
+
+# # Wat gebeurt hier nu weer:
+# print( 3 * "hallo" )
+
+# btw opdracht 
+a = 3.40 + 2.45 + 1.95
+b = 109
+
+
+print ( a / b * 9 ) 
