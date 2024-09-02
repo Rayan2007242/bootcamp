1d147cf4871553bb66f2a9ea738275c273ce2a8e
Rayan2007242 <rayan3062007@gmail.com>
2024-09-02 13:42:18 +0200

    gevuld

---
+++ bc_l3_doemee.py
@@ -0,0 +1,17 @@
+# Wat gaat er gebeuren?
+getal1 = 25
+getal2 = 5
+
+print (f"{getal1}/{getal2}={getal1/getal2}")
+
+
+# we gaan hier een variable inbouwen
+naam= input('Wat is je naam?')
+print(f'Hallo {naam}, ik leer nu programmeren.')
+
+
+# we schrijven code dat de gebruiker vraagt om een woord in te voeren
+# en vervolgens de volgende zin print:
+woord = input(" Geef een woord ")
+aantal_tekens = len(woord)
+print (f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.")
--- bc_l3_opdracht1.py
+++ bc_l3_opdracht1.py
@@ -1,8 +1,8 @@
 description = 'dag'
 
-days = 
-hours = days * 
-minutes = hours * 
-seconds = minutes * 
+days = 1
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
 
-print(f'In een {description} zitten {seconds} seconden')
+print(f'In een {description} zitten {seconds} seconden') 
